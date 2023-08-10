4f072671d7ea443df03a3643ee6e9148fa94ff33
rulanugrh <nextarituenak01@gmail.com>
2023-08-10 10:27:58 +0700

    feat: user handler

---
+++ handler/port/user.interface.go
@@ -0,0 +1,11 @@
+package port
+
+import "net/http"
+
+type UserHandlerInterface interface {
+	Register(w http.ResponseWriter, r *http.Request)
+	Login(w http.ResponseWriter, r *http.Request)
+	UpdateAccount(w http.ResponseWriter, r *http.Request)
+	DeleteAccount(w http.ResponseWriter, r *http.Request)
+	
+}
---
+++ handler/user.handler.go
@@ -0,0 +1,154 @@
+package handler
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+	"github.com/rulanugrh/orion/entity/domain"
+	"github.com/rulanugrh/orion/entity/web"
+	portHand "github.com/rulanugrh/orion/handler/port"
+	"github.com/rulanugrh/orion/middleware"
+	"github.com/rulanugrh/orion/services/port"
+)
+
+type userhandler struct {
+	userServ port.UserServiceInterface
+}
+
+func NewUserHandler(user port.UserServiceInterface) portHand.UserHandlerInterface {
+	return &userhandler{
+		userServ: user,
+	}
+}
+func (hnd *userhandler) Register(w http.ResponseWriter, r*http.Request){
+	var req domain.UserEntity
+	data, _ := ioutil.ReadAll(r.Body)
+
+	json.Unmarshal(data, &req)
+	password := middleware.HashPassword(req.Password)
+	req.Password = password
+
+	_, err := hnd.userServ.Register(req)
+	if err != nil {
+		res := web.ResponseFailure {
+			Message: "Cant Login",
+		}
+		log.Printf("cant login because: %v", err)
+		response, _ := json.Marshal(res)
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write(response)
+	}
+
+	token, errToken := middleware.GenerateToken(req)
+	if errToken != nil {
+		res := web.ResponseFailure {
+			Message: "Cant Generate Token",
+		}
+		log.Printf("cant generate token because: %v", errToken)
+		response, _ := json.Marshal(res)
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write(response)
+	}
+
+	res := web.ResponseSuccess {
+		Message: "success register account",
+		Data: token,
+	}
+	response, _ := json.Marshal(res)
+	w.WriteHeader(http.StatusOK)
+	w.Write(response)
+}
+
+func (hnd *userhandler) Login(w http.ResponseWriter, r*http.Request) {
+	var req domain.UserEntity
+	data, _ := ioutil.ReadAll(r.Body)
+
+	json.Unmarshal(data, &req)
+	result, err := hnd.userServ.FindByEmail(req.Email)
+	if err != nil {
+		res := web.ResponseFailure {
+			Message: "Cant Find Account with this Email",
+		}
+		log.Printf("cant find account because: %v", err)
+		response, _ := json.Marshal(res)
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write(response)
+	}
+
+	compare := []byte(req.Password)
+	if errCheck := middleware.ComparePassword(req.Password, compare); errCheck != nil {
+		res := web.ResponseFailure {
+			Message: "Password not matched",
+		}
+		log.Printf("password not matched because: %v", err)
+		response, _ := json.Marshal(res)
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write(response)
+	}
+
+	res := web.ResponseSuccess {
+		Message: "success login account",
+		Data: result,
+	}
+	response, _ := json.Marshal(res)
+	w.WriteHeader(http.StatusOK)
+	w.Write(response)
+
+}
+
+func (hnd *userhandler) UpdateAccount(w http.ResponseWriter, r*http.Request) {
+	getId := mux.Vars(r)
+	params := getId["id"]
+	id, _ := strconv.Atoi(params)
+
+	var req domain.UserEntity
+	data, _ := ioutil.ReadAll(r.Body)
+	json.Unmarshal(data, &req)
+
+	result, err := hnd.userServ.Update(uint(id), req)
+	if err != nil {
+		res := web.ResponseFailure {
+			Message: "Cant Update Account with this ID",
+		}
+		log.Printf("cant update account because: %v", err)
+		response, _ := json.Marshal(res)
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write(response)
+	}
+
+	res := web.ResponseSuccess {
+		Message: "success login account",
+		Data: result,
+	}
+	response, _ := json.Marshal(res)
+	w.WriteHeader(http.StatusOK)
+	w.Write(response)
+}
+
+func (hnd *userhandler) DeleteAccount(w http.ResponseWriter, r*http.Request) {
+	getId := mux.Vars(r)
+	params := getId["id"]
+	id, _ := strconv.Atoi(params)
+
+	if err := hnd.userServ.DeleteAccount(uint(id)); err != nil {
+		res := web.ResponseFailure {
+			Message: "Cant Delete Account with this ID",
+		}
+		log.Printf("cant delete account because: %v", err)
+		response, _ := json.Marshal(res)
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write(response)
+	}
+
+	res := web.ResponseSuccess {
+		Message: "success login account",
+		Data: "Account with this ID has been delete",
+	}
+	response, _ := json.Marshal(res)
+	w.WriteHeader(http.StatusOK)
+	w.Write(response)
+}
