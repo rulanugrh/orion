5fdd22fa126a13ee51da0f78ea448cb555ab9e22
rulanugrh <nextarituenak01@gmail.com>
2023-08-11 19:06:39 +0700

    feat: detail validate

--- entity/domain/user.entity.go
+++ entity/domain/user.entity.go
@@ -6,7 +6,7 @@ type UserEntity struct {
 	gorm.Model
 	Name     string `json:"name" form:"name" validate:"required"`
 	Age      int    `json:"age" form:"age" validate:"required"`
-	Email    string `json:"email" form:"email" validate:"required"`
+	Email    string `json:"email" form:"email" validate:"required,email"`
 	Password string `json:"password" form:"password" validate:"required"`
 	Notelp   string `json:"notelp" form:"notelp" validate:"required"`
 	Address  string `json:"address" form:"address" validate:"required"`
